2d4104c2291e5995788e4258d46cda847e076032
Robert Jacobsen <robert@robertjacobsen.net>
2009-09-27 17:29:37 +0200

    Initial commit \o/

---
+++ font.lua
@@ -0,0 +1,9 @@
+ChatFontNormal:SetFont(STANDARD_TEXT_FONT, 13)
+SystemFont_Shadow_Small:SetFont(select(1, SystemFont_Shadow_Small:GetFont()), 11)
+CHAT_FONT_HEIGHTS = {
+	[1] = 10, 
+	[2] = 12,
+	[3] = 13, 
+	[4] = 14,
+	[5] = 16
+}
